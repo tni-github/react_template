1b8b7b925643e0fcccfa079b6aa0689ea46f30e0
tni <tni_consultant@mail.ru>
2022-09-09 22:02:55 +0300

    react template downloaded first time

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import picture from './img/IMG_20200729_073755.jpg'
+
+const Module = () => (
+    <>
+        <h1>Hello, React</h1>
+        <img src={picture} width={300} />
+    </>
+)
+
+export default Module;
